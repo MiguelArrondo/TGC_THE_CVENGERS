18a64b7a2c074444463f86454f307a65fd504c01
Miguel Arrondo <Miguel Arrondo>
2016-05-08 16:08:55 -0300

    fix colision

--- MiGrupo/FPSCustomCamera.cs
+++ MiGrupo/FPSCustomCamera.cs
@@ -545,7 +545,7 @@ namespace AlumnoEjemplos.MiGrupo
 
                 if (direction.Z < 0.0f //&& Math.Abs(currentVelocity.Z) < 1e-6f
                    )
-                    displacement.Z =-(displacement.Z / 3);
+                    displacement.Z =-displacement.Z;
                 if (direction.Y < 0.0f //&& Math.Abs(currentVelocity.Y) < 1e-6f
                     )
                     displacement.Y =-displacement.Y;
